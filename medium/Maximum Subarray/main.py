3a5c1df9bddc3b919a80b801841af63e5bec3b83
traitimtrongvag <tbinh831@gmail.com>
2025-11-19 21:52:41 +0700

    update

--- medium/Maximum Subarray/main.py
+++ medium/Maximum Subarray/main.py
@@ -1,1 +1,11 @@
 # 53. Maximum Subarray
+class Solution:
+    def maxSubArray(self, nums):
+        current_sum = 0
+        max_sum = nums[0]
+
+        for x in nums:
+            current_sum = max(x, current_sum + x)
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
