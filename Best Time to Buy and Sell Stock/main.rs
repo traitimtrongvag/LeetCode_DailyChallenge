e0403dea3ddccfdfdef16181237e9cb12caf6828
traitimtrongvag <tbinh831@gmail.com>
2025-11-19 19:14:47 +0700

    update

--- Best Time to Buy and Sell Stock/main.rs
+++ Best Time to Buy and Sell Stock/main.rs
@@ -11,3 +11,24 @@ impl Solution {
         max_profit
     }
 }
+/*
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut min_price = prices[0];
+        let mut max_profit = 0;
+
+        for price in prices {
+            if price < min_price {
+                min_price = price;
+            } else {
+                let profit = price - min_price;
+                if profit > max_profit {
+                    max_profit = profit;
+                }
+            }
+        }
+
+        max_profit
+    }
+}	
+*/
