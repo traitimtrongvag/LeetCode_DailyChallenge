78928eae92bc8f0006c518d09c6e0b38590d4505
traitimtrongvag <tbinh831@gmail.com>
2025-11-19 21:53:51 +0700

    update

--- medium/Maximum Subarray/main.rs
+++ medium/Maximum Subarray/main.rs
@@ -1,1 +1,14 @@
 // 53. Maximum Subarray
+impl Solution {
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        let mut current_sum = 0;
+        let mut max_sum = nums[0];
+
+        for &x in &nums {
+            current_sum = (current_sum + x).max(x);
+            max_sum = max_sum.max(current_sum);
+        }
+
+        max_sum
+    }
+}
