cd5c7c23ddc5c8f59d3645bbc1f47b062586504d
traitimtrongvag <tbinh831@gmail.com>
2025-11-20 17:25:35 +0700

    update

--- easy/Plus One/main.java
+++ easy/Plus One/main.java
@@ -1,1 +1,17 @@
 // 66. Plus one
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int i = digits.length;
+        while (i > 0) {
+            i--;
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] r = new int[digits.length + 1];
+        r[0] = 1;
+        return r;
+    }
+}
