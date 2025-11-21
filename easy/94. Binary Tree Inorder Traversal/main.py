8a4d86d2c052e650ef6665115307649f4b07254b
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 00:39:24 +0700

    Update

--- easy/94. Binary Tree Inorder Traversal/main.py
+++ easy/94. Binary Tree Inorder Traversal/main.py
@@ -1,1 +1,13 @@
 # 94. Binary Tree Inorder Traversal
+class Solution:
+    def inorderTraversal(self, root):
+        res, st = [], []
+        cur = root
+        while cur or st:
+            while cur:
+                st.append(cur)
+                cur = cur.left
+            cur = st.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res
