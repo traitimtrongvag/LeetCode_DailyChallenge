a5ba8e669747b15f902a580314fe21a12d103576
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 00:27:36 +0700

    Update

--- easy/27. Remove Element/main.rs
+++ easy/27. Remove Element/main.rs
@@ -12,3 +12,11 @@ impl Solution {
         k as i32
     }
 }
+/*
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        nums.retain(|&x| x != val);
+        nums.len() as i32
+    }
+}
+*/
