3196e115cbdbea89f077cd62c70f5fb0844df1ff
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 00:22:52 +0700

    Update

--- easy/27. Remove Element/main.rs
+++ easy/27. Remove Element/main.rs
@@ -1,1 +1,15 @@
 // 27. Remove Element
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut k = 0; // Pointer for next position to place valid element
+        
+        for i in 0..nums.len() {
+            if nums[i] != val {
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+        
+        k as i32
+    }
+}
