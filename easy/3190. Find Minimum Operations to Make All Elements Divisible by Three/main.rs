c5dc9c69ad9b56d62ec33074e44ab0e9a3e7fce0
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 10:50:59 +0700

    Update

--- easy/3190. Find Minimum Operations to Make All Elements Divisible by Three/main.rs
+++ easy/3190. Find Minimum Operations to Make All Elements Divisible by Three/main.rs
@@ -1,1 +1,11 @@
 // 3190. Find Minimum Operations to Make All Elements Divisible by Three
+impl Solution {
+    pub fn minimum_operations(nums: Vec<i32>) -> i32 {
+        let mut operations = 0;
+        for &num in &nums {
+            let remainder = num % 3;
+            operations += remainder.min(3 - remainder);
+        }
+        operations
+    }
+}
