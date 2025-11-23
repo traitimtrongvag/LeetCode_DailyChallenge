5f48d2e3e85c88a77edcf516e0d5fae11cf0223f
tbinh831 <tbinh831@gmail.com>
2025-11-23 18:53:42 +0700

    Update

--- easy/2094. Finding 3-Digit Even Numbers/main.cpp
+++ easy/2094. Finding 3-Digit Even Numbers/main.cpp
@@ -1,1 +1,28 @@
 // 2094. Finding 3-Digit Even Numbers
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> count(10, 0);
+        for (int d : digits) count[d]++;
+        
+        vector<int> result;
+        for (int num = 100; num <= 998; num += 2) {
+            vector<int> tempCount(10, 0);
+            int n = num;
+            while (n > 0) {
+                tempCount[n % 10]++;
+                n /= 10;
+            }
+            
+            bool valid = true;
+            for (int i = 0; i < 10; i++) {
+                if (tempCount[i] > count[i]) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) result.push_back(num);
+        }
+        return result;
+    }
+};
