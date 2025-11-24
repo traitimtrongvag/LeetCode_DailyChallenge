73778438a458d0d16a8d35f24e6d79d6b089bc9f
tbinh831 <tbinh831@gmail.com>
2025-11-24 21:08:05 +0700

    Update

--- easy/222. Count Complete Tree Nodes/main.java
+++ easy/222. Count Complete Tree Nodes/main.java
@@ -1,1 +1,24 @@
 // 222. Count Complete Tree Nodes
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        int leftHeight = 0, rightHeight = 0;
+        TreeNode left = root;
+        TreeNode right = root;
+        
+        while (left != null) {
+            leftHeight++;
+            left = left.left;
+        }
+        while (right != null) {
+            rightHeight++;
+            right = right.right;
+        }
+        
+        if (leftHeight == rightHeight) {
+            return (1 << leftHeight) - 1;
+        }
+        
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
