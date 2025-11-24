ff53cb3977a7432962ac30d121021ea9e1e54c07
tbinh831 <tbinh831@gmail.com>
2025-11-24 11:26:33 +0700

    Update

--- easy/1018. Binary Prefix Divisible By 5/main.cpp
+++ easy/1018. Binary Prefix Divisible By 5/main.cpp
@@ -1,1 +1,15 @@
 // 1018. Binary Prefix Divisible By 5
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> result;
+        int current = 0;
+        
+        for (int num : nums) {
+            current = (current * 2 + num) % 5;
+            result.push_back(current == 0);
+        }
+        
+        return result;
+    }
+};
