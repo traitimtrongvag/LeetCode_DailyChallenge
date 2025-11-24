c4fc1d14c7f81f4f35f86a3b5f951c37c1ce5ed6
tbinh831 <tbinh831@gmail.com>
2025-11-24 20:13:50 +0700

    Update

--- easy/14. Longest Common Prefix/main.rs
+++ easy/14. Longest Common Prefix/main.rs
@@ -1,1 +1,21 @@
 // 14. Longest Common Prefix
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        if strs.is_empty() {
+            return String::new();
+        }
+        
+        let first = strs[0].as_bytes();
+        for i in 0..first.len() {
+            let c = first[i];
+            for s in &strs[1..] {
+                let bytes = s.as_bytes();
+                if i >= bytes.len() || bytes[i] != c {
+                    return String::from_utf8(first[..i].to_vec()).unwrap();
+                }
+            }
+        }
+        
+        strs[0].clone()
+    }
+}
