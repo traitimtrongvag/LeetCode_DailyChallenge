01a88533b070c066cc5ddba513361c5b8c3721a2
tbinh831 <tbinh831@gmail.com>
2025-11-24 11:26:12 +0700

    Update

--- easy/1018. Binary Prefix Divisible By 5/main.java
+++ easy/1018. Binary Prefix Divisible By 5/main.java
@@ -1,1 +1,14 @@
 // 1018. Binary Prefix Divisible By 5
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> result = new ArrayList<>();
+        int current = 0;
+        
+        for (int num : nums) {
+            current = (current * 2 + num) % 5;
+            result.add(current == 0);
+        }
+        
+        return result;
+    }
+}
