542d01a3acb195153f6433d3a1ec2093c58ec1ba
tbinh831 <tbinh831@gmail.com>
2025-11-25 22:59:23 +0700

    Update

--- easy/20. Valid Parentheses/main.rs
+++ easy/20. Valid Parentheses/main.rs
@@ -1,1 +1,16 @@
 // 20. Valid Parentheses
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for c in s.chars() {
+            match c {
+                '(' | '[' | '{' => stack.push(c),
+                ')' => if stack.pop() != Some('(') { return false },
+                ']' => if stack.pop() != Some('[') { return false },
+                '}' => if stack.pop() != Some('{') { return false },
+                _ => return false
+            }
+        }
+        stack.is_empty()
+    }
+}
