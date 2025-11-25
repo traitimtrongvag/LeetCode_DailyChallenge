1d0eef02df5c50558cbc16d37b4aaeacc1b38165
tbinh831 <tbinh831@gmail.com>
2025-11-25 20:01:40 +0700

    Update

--- medium/11. Container With Most Water/main.c
+++ medium/11. Container With Most Water/main.c
@@ -1,1 +1,15 @@
 // 11. Container With Most Water
+int maxArea(int* height, int heightSize) {
+    int left = 0, right = heightSize - 1;
+    int maxArea = 0;
+    while (left < right) {
+        int area = fmin(height[left], height[right]) * (right - left);
+        if (area > maxArea) maxArea = area;
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxArea;
+}
