28f2eebe80efd199f9ee4c1af0a50e839e049877
tbinh831 <tbinh831@gmail.com>
2025-11-25 21:20:36 +0700

    Update

--- easy/594. Longest Harmonious Subsequence/main.rs
+++ easy/594. Longest Harmonious Subsequence/main.rs
@@ -1,1 +1,19 @@
 // 594. Longest Harmonious Subsequence
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn find_lhs(nums: Vec<i32>) -> i32 {
+        let mut freq = HashMap::new();
+        for &num in &nums {
+            *freq.entry(num).or_insert(0) += 1;
+        }
+        
+        let mut max_len = 0;
+        for (&num, &count) in &freq {
+            if let Some(&next_count) = freq.get(&(num + 1)) {
+                max_len = max_len.max(count + next_count);
+            }
+        }
+        max_len
+    }
+}
