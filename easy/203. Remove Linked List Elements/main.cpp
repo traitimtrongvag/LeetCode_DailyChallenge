01afeee2b69bc4d626fdd48bb38ee4af47ad01d7
tbinh831 <tbinh831@gmail.com>
2025-11-25 22:49:05 +0700

    Update

--- easy/203. Remove Linked List Elements/main.cpp
+++ easy/203. Remove Linked List Elements/main.cpp
@@ -1,1 +1,17 @@
 // 203. Remove Linked List Elements
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* curr = dummy;
+        
+        while (curr->next) {
+            if (curr->next->val == val) {
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
