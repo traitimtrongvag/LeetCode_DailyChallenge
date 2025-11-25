0f813522a87cf3732e9f219f37ec464c80a44c6c
tbinh831 <tbinh831@gmail.com>
2025-11-25 20:01:27 +0700

    Update

--- medium/11. Container With Most Water/main.rs
+++ medium/11. Container With Most Water/main.rs
@@ -1,1 +1,21 @@
 // 11. Container With Most Water
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = height.len() - 1;
+        let mut max_area = 0;
+        
+        while left < right {
+            let area = height[left].min(height[right]) * (right - left) as i32;
+            max_area = max_area.max(area);
+            
+            if height[left] < height[right] {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        
+        max_area
+    }
+}
