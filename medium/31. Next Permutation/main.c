e3f88b6af8c89d4f98d4743c007d9785d02efd7b
tbinh831 <tbinh831@gmail.com>
2025-11-26 17:24:09 +0700

    Update

--- medium/31. Next Permutation/main.c
+++ medium/31. Next Permutation/main.c
@@ -1,1 +1,20 @@
 // 31. Next Permutation
+void nextPermutation(int* nums, int numsSize) {
+    int i = numsSize - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+    if (i >= 0) {
+        int j = numsSize - 1;
+        while (nums[j] <= nums[i]) j--;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    int left = i + 1, right = numsSize - 1;
+    while (left < right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--;
+    }
+}
