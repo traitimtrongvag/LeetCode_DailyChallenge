cebf164410150e678b688f9408c416c40dc98f1d
traitimtrongvag <tbinh831@gmail.com>
2025-11-28 12:52:44 +0700

    Update

--- medium/56. Merge Intervals/main.java
+++ medium/56. Merge Intervals/main.java
@@ -1,1 +1,21 @@
 // 56. Merge Intervals
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) return new int[0][];
+        
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+        merged.add(intervals[0]);
+        
+        for (int i = 1; i < intervals.length; i++) {
+            int[] last = merged.get(merged.size() - 1);
+            if (intervals[i][0] <= last[1]) {
+                last[1] = Math.max(last[1], intervals[i][1]);
+            } else {
+                merged.add(intervals[i]);
+            }
+        }
+        
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
