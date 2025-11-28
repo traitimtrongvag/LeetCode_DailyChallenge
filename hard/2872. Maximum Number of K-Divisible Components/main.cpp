7dd888106be0d3033db09632981537bd434592c6
traitimtrongvag <tbinh831@gmail.com>
2025-11-28 11:29:18 +0700

    Update

--- hard/2872. Maximum Number of K-Divisible Components/main.cpp
+++ hard/2872. Maximum Number of K-Divisible Components/main.cpp
@@ -1,1 +1,29 @@
 // 2872. Maximum Number of K-Divisible Components
+class Solution {
+public:
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        vector<vector<int>> graph(n);
+        for (auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        
+        int components = 0;
+        function<long long(int, int)> dfs = [&](int node, int parent) -> long long {
+            long long sum = values[node];
+            for (int neighbor : graph[node]) {
+                if (neighbor != parent) {
+                    sum += dfs(neighbor, node);
+                }
+            }
+            if (sum % k == 0) {
+                components++;
+                return 0;
+            }
+            return sum;
+        };
+        
+        dfs(0, -1);
+        return components;
+    }
+};
