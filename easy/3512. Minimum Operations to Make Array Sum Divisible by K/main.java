2093a65afebc46920d550cfc92be680f4ec0fbd3
traitimtrongvag <tbinh831@gmail.com>
2025-11-29 23:26:45 +0700

    Update

--- easy/3512. Minimum Operations to Make Array Sum Divisible by K/main.java
+++ easy/3512. Minimum Operations to Make Array Sum Divisible by K/main.java
@@ -1,1 +1,12 @@
 // 3512. Minimum Operations to Make Array Sum Divisible by K
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        long sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        
+        int remainder = (int)(sum % k);
+        return remainder;
+    }
+}
