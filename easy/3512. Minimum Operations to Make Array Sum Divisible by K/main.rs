7e8a7dcda6c586fb0a2fd9757b97892ba56db7a1
traitimtrongvag <tbinh831@gmail.com>
2025-11-29 23:28:08 +0700

    Update

--- easy/3512. Minimum Operations to Make Array Sum Divisible by K/main.rs
+++ easy/3512. Minimum Operations to Make Array Sum Divisible by K/main.rs
@@ -1,1 +1,7 @@
 // 3512. Minimum Operations to Make Array Sum Divisible by K
+impl Solution {
+    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let sum: i64 = nums.iter().map(|&x| x as i64).sum();
+        (sum % k as i64) as i32
+    }
+}
