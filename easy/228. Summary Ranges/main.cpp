e7bf5a9ad1ba29938bcc4e4e24b628e1199ad54a
traitimtrongvag <tbinh831@gmail.com>
2025-11-29 23:19:52 +0700

    Update

--- easy/228. Summary Ranges/main.cpp
+++ easy/228. Summary Ranges/main.cpp
@@ -1,1 +1,24 @@
 // 228. Summary Ranges
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> result;
+        int n = nums.size();
+        if (n == 0) return result;
+        
+        int start = nums[0];
+        for (int i = 1; i <= n; i++) {
+            if (i < n && nums[i] == nums[i - 1] + 1) {
+                continue;
+            } else {
+                if (start == nums[i - 1]) {
+                    result.push_back(to_string(start));
+                } else {
+                    result.push_back(to_string(start) + "->" + to_string(nums[i - 1]));
+                }
+                if (i < n) start = nums[i];
+            }
+        }
+        return result;
+    }
+};
