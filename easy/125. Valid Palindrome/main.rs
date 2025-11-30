e348993ba7dc3bae6cb3dd1764c1e442d5d7359e
traitimtrongvag <tbinh831@gmail.com>
2025-11-30 21:09:02 +0700

    Update

--- easy/125. Valid Palindrome/main.rs
+++ easy/125. Valid Palindrome/main.rs
@@ -1,1 +1,12 @@
 // 125. Valid Palindrome
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        let filtered: Vec<char> = s
+            .chars()
+            .filter(|c| c.is_ascii_alphanumeric())
+            .map(|c| c.to_ascii_lowercase())
+            .collect();
+        
+        filtered.iter().eq(filtered.iter().rev())
+    }
+}
