3dbc226b8e19ff7df7257944179671711b6feb9f
traitimtrongvag <tbinh831@gmail.com>
2025-12-01 12:32:50 +0700

    Update

--- easy/67. Add Binary/main.java
+++ easy/67. Add Binary/main.java
@@ -1,1 +1,16 @@
 // 67. Add Binary
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            result.insert(0, sum % 2);
+            carry = sum / 2;
+        }
+        return result.toString();
+    }
+}
