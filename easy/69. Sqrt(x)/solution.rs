3f6d309459746732ccd277ab462748107af9d3dd
traitimtrongvag <tbinh831@gmail.com>
2025-12-02 21:17:29 +0700

    Update

--- easy/69. Sqrt(x)/solution.rs
+++ easy/69. Sqrt(x)/solution.rs
@@ -1,1 +1,22 @@
 // 69. Sqrt(x)
+impl Solution {
+    pub fn my_sqrt(x: i32) -> i32 {
+        if x == 0 {
+            return 0;
+        }
+        
+        let mut left = 1;
+        let mut right = x;
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            if mid == x / mid {
+                return mid;
+            } else if mid > x / mid {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        right
+    }
+}
