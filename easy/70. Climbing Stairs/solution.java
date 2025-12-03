19a812137899a50162919d427c680946d6bd4f8c
traitimtrongvag <tbinh831@gmail.com>
2025-12-03 20:06:07 +0700

    Update

--- easy/70. Climbing Stairs/solution.java
+++ easy/70. Climbing Stairs/solution.java
@@ -1,1 +1,24 @@
 // 70. Climbing Stairs
+class Solution {
+    public static void helper(int n , int count[],HashMap<Integer,Integer> map ){
+        if(n < 0 ) return ;
+        if(n == 0){
+            count[0] = count[0] + 1;
+            return; }
+
+        if(map.containsKey(n-1)) count[0] = count[0] + map.get(n-1);
+        else helper(n - 1, count,map);
+        
+        if(map.containsKey(n-2)) count[0] = count[0] + map.get(n-2);
+        else helper(n - 2, count,map);
+
+        map.put(n,count[0]);
+    }
+
+    public int climbStairs(int n) {
+        int count[] = {0};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        helper(n,count,map);
+        return count[0];
+    }
+}
