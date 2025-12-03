4e8a3334bbe4a6512d603a8f06b05f1a03d73db6
traitimtrongvag <tbinh831@gmail.com>
2025-12-03 20:07:55 +0700

    Update

--- easy/70. Climbing Stairs/solution.rs
+++ easy/70. Climbing Stairs/solution.rs
@@ -1,1 +1,12 @@
 // 70. Climbing Stairs
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let (mut a, mut b) = (1, 1);
+        for _ in 0..(n - 1) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        b
+    }
+}
