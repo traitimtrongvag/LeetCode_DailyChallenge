e2087d0e1742796931e0554551ee80eb5aad5e7d
traitimtrongvag <tbinh831@gmail.com>
2025-12-04 22:08:43 +0700

    Update

--- easy/83. Remove Duplicates from Sorted List/solution.java
+++ easy/83. Remove Duplicates from Sorted List/solution.java
@@ -1,1 +1,16 @@
 // 83. Remove Duplicates from Sorted List
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode res = head;
+
+        while (head != null && head.next != null) {
+            if (head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+
+        return res;        
+    }
+}
