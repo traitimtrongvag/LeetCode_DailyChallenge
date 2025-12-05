93c2bf1fb4ff0675da82b8e45b55d782640ed43f
traitimtrongvag <tbinh831@gmail.com>
2025-12-05 18:03:13 +0700

    Update

--- easy/3432. Count Partitions with Even Sum Difference/solution.rs
+++ easy/3432. Count Partitions with Even Sum Difference/solution.rs
@@ -1,1 +1,16 @@
 // 3432. Count Partitions with Even Sum Difference
+impl Solution {
+    pub fn count_partitions(nums: Vec<i32>) -> i32 {
+        let total: i32 = nums.iter().sum();
+        let mut left = 0;
+        let mut ans = 0;
+        for i in 0..nums.len() - 1 {
+            left += nums[i];
+            let right = total - left;
+            if ((left - right) & 1) == 0 {
+                ans += 1;
+            }
+        }
+        ans
+    }
+}
