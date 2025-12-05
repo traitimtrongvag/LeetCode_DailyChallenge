91ef4ae2d3aba075058f362845f4b8f75088495f
traitimtrongvag <tbinh831@gmail.com>
2025-12-05 18:03:27 +0700

    Update

--- easy/3432. Count Partitions with Even Sum Difference/solution.java
+++ easy/3432. Count Partitions with Even Sum Difference/solution.java
@@ -1,1 +1,14 @@
 // 3432. Count Partitions with Even Sum Difference
+class Solution {
+    public int countPartitions(int[] nums) {
+        int total = 0;
+        for (int x : nums) total += x;
+        int left = 0, ans = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            left += nums[i];
+            int right = total - left;
+            if (((left - right) & 1) == 0) ans++;
+        }
+        return ans;
+    }
+}
