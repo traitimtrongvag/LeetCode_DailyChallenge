5a65951b6645546936dcfff65c4ae7c6e5763213
traitimtrongvag <193246389+traitimtrongvag@users.noreply.github.com>
2026-01-02 21:24:04 +0700

    Update

--- easy/961. N-Repeated Element in Size 2N Array/solution.java
+++ easy/961. N-Repeated Element in Size 2N Array/solution.java
@@ -2,10 +2,9 @@ class Solution {
     public int repeatedNTimes(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] == nums[i + 1]) return nums[i];
-            if (i + 2 < n)
+            if (i + 2 < nums.length)
                 if (nums[i] == nums[i + 2]) return nums[i];
         }
-        
         return nums[0];
     }
 }
