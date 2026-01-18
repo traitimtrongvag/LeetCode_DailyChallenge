1654e11b74903b8ad51b3a92a0ad2b106316d89b
traitimtrongvag <193246389+traitimtrongvag@users.noreply.github.com>
2026-01-18 23:00:00 +0700

    style(easy/3131): add explanatory comments

--- easy/3131. Find the Integer Added to Array I/solution.java
+++ easy/3131. Find the Integer Added to Array I/solution.java
@@ -1,11 +1,22 @@
+// Algorithm overview:
+// All elements in nums1 are shifted by the same value to form nums2.
+// That value can be determined by comparing the minimum elements.
+// Compute the minimum of both arrays and return their difference.
+
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
-        int min1=Integer.MAX_VALUE;
-        int min2=Integer.MAX_VALUE;
-        for(int i=0;i<nums1.length;i++){
-            min1= nums1[i]<min1 ?nums1[i]:min1;
-            min2= nums2[i]<min2 ?nums2[i]:min2;
+        // Track the minimum value in nums1.
+        int min1 = Integer.MAX_VALUE;
+        // Track the minimum value in nums2.
+        int min2 = Integer.MAX_VALUE;
+
+        // Find the minimum values in both arrays.
+        for (int i = 0; i < nums1.length; i++) {
+            min1 = nums1[i] < min1 ? nums1[i] : min1;
+            min2 = nums2[i] < min2 ? nums2[i] : min2;
         }
-        return min2-min1;
+
+        // The added integer is the difference between the minimum values.
+        return min2 - min1;
     }
 }
