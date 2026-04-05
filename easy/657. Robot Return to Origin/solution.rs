3739fbda3e4f802fcdf56dbfbd8f0e15234c8431
traitimtrongvag <pan2512811@gmail.com>
2026-04-05 22:03:45 +0700

    feat(leetcode): add Rust solution for 657. Robot Return to Origin

---
+++ easy/657. Robot Return to Origin/solution.rs
@@ -0,0 +1,21 @@
+// simple: track x, y coordinates
+// 'U' +1 y, 'D' -1 y, 'L' -1 x, 'R' +1 x
+
+impl Solution {
+    pub fn judge_circle(moves: String) -> bool {
+        let mut x = 0;
+        let mut y = 0;
+
+        for c in moves.chars() {
+            match c {
+                'U' => y += 1,
+                'D' => y -= 1,
+                'L' => x -= 1,
+                'R' => x += 1,
+                _ => (),
+            }
+        }
+
+        x == 0 && y == 0
+    }
+}
