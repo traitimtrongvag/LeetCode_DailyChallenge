e793b8384f336ac5794b03d6109e9a8659f8064a
traitimtrongvag <193246389+traitimtrongvag@users.noreply.github.com>
2026-04-08 14:12:32 +0000

    style: reformat Two Sum Java - spacing, imports, names (#16)

--- easy/1. Two Sum/solution.java
+++ easy/1. Two Sum/solution.java
@@ -1,10 +1,14 @@
-class Solution{
-    public int[] twoSum(int[] nums,int target){
-        java.util.HashMap<Integer,Integer> m=new java.util.HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int c=target-nums[i];
-            if(m.containsKey(c))return new int[]{m.get(c),i};
-            m.put(nums[i],i);
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
         }
         return new int[0];
     }
