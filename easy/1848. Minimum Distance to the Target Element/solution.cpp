984fff5d938b97ae4d6d599e226beb52999062a7
traitimtrongvag <193246389+traitimtrongvag@users.noreply.github.com>
2026-04-13 20:35:08 +0700

    feat(leetcode): add C++ solution for 1848. Minimum Distance to the Target Element

---
+++ easy/1848. Minimum Distance to the Target Element/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int ans = 1e9;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                ans = min(ans, abs(i - start));
+            }
+        }
+
+        return ans;
+    }
+};
