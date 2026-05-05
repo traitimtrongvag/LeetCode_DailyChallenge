62f6168de23361f49b09a0beaaae0fc069f9eb84
pan2512811 <193246389+traitimtrongvag@users.noreply.github.com>
2026-05-05 21:41:16 +0700

    refactor: clean code and add comments

--- easy/1018. Binary Prefix Divisible By 5/solution.cpp
+++ easy/1018. Binary Prefix Divisible By 5/solution.cpp
@@ -1,12 +1,22 @@
+/*
+Time: O(n)
+Space: O(1) (excluding output)
+
+Idea: build binary prefix mod 5, only keep remainder
+*/
+
 class Solution {
 public:
     vector<bool> prefixesDivBy5(vector<int>& nums) {
         vector<bool> result;
-        int current = 0;
+        int cur = 0; // current value mod 5
         
-        for (int num : nums) {
-            current = (current * 2 + num) % 5;
-            result.push_back(current == 0);
+        for (int bit : nums) {
+            // shift left (x2) and add new bit, keep mod 5
+            cur = (cur * 2 + bit) % 5;
+
+            // divisible by 5 if remainder == 0
+            result.push_back(cur == 0);
         }
         
         return result;
