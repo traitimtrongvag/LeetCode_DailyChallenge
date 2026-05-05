b898494a9f7085ef2c821472a7ec88d37d18fd06
pan2512811 <193246389+traitimtrongvag@users.noreply.github.com>
2026-05-05 21:56:14 +0700

    refactor: clean code and add comments

--- easy/1018. Binary Prefix Divisible By 5/solution.rs
+++ easy/1018. Binary Prefix Divisible By 5/solution.rs
@@ -1,13 +1,23 @@
+/*
+Time: O(n)
+Space: O(1) (excluding output)
+
+Idea: build binary prefix mod 5, only keep remainder
+*/
+
 impl Solution {
     pub fn prefixes_div_by5(nums: Vec<i32>) -> Vec<bool> {
-        let mut result = Vec::with_capacity(nums.len());
-        let mut current = 0;
+        let mut res = Vec::with_capacity(nums.len());
+        let mut cur = 0; // current value mod 5
         
-        for &num in &nums {
-            current = (current * 2 + num) % 5;
-            result.push(current == 0);
+        for &bit in &nums {
+            // shift left (x2) and add new bit, keep mod 5
+            cur = (cur * 2 + bit) % 5;
+
+            // divisible by 5 if remainder == 0
+            res.push(cur == 0);
         }
         
-        result
+        res
     }
 }
