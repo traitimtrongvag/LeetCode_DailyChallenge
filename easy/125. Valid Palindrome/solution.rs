c470bd437be19361a3ff39d3a6490abd5ce5fa5d
pan2512811 <193246389+traitimtrongvag@users.noreply.github.com>
2026-05-06 22:01:39 +0700

    refactor: clean code

--- easy/125. Valid Palindrome/solution.rs
+++ easy/125. Valid Palindrome/solution.rs
@@ -1,11 +1,18 @@
+/*
+Time: O(n)
+Space: O(n)
+
+Idea: filter + lowercase, then check palindrome
+*/
+
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let filtered: Vec<char> = s
+        let v: Vec<char> = s
             .chars()
             .filter(|c| c.is_ascii_alphanumeric())
             .map(|c| c.to_ascii_lowercase())
             .collect();
-        
-        filtered.iter().eq(filtered.iter().rev())
+
+        v.iter().eq(v.iter().rev())
     }
 }
