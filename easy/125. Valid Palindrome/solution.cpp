026caed4f2f0d5033fc40c8e2a27819c50141edb
pan2512811 <193246389+traitimtrongvag@users.noreply.github.com>
2026-05-06 22:01:39 +0700

    refactor: clean code

--- easy/125. Valid Palindrome/solution.cpp
+++ easy/125. Valid Palindrome/solution.cpp
@@ -1,13 +1,23 @@
+/*
+Time: O(n)
+Space: O(1)
+
+Idea: two pointers, skip non-alnum, compare lowercase
+*/
+
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0, right = s.size() - 1;
-        while (left < right) {
-            while (left < right && !isalnum(s[left])) left++;
-            while (left < right && !isalnum(s[right])) right--;
-            if (tolower(s[left]) != tolower(s[right])) return false;
-            left++;
-            right--;
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) l++;
+            while (l < r && !isalnum(s[r])) r--;
+
+            if (tolower(s[l]) != tolower(s[r])) return false;
+
+            l++;
+            r--;
         }
         return true;
     }
