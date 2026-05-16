ea558b4581ae7b6bac506e6fc44ba09479a8c43a
pan2512811 <pan2512811@gmail.com>
2026-05-16 19:32:12 +0700

    feat(leetcode): add C++ solution for 154. Find Minimum in Rotated Sorted Array II

---
+++ hard/154. Find Minimum in Rotated Sorted Array II/solution.cpp
@@ -0,0 +1,30 @@
+/*
+Time: O(log n) average, O(n) worst
+Space: O(1)
+
+Idea: binary search, shrink right when duplicates appear
+*/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] < nums[r]) {
+                r = mid;
+            }
+            else if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            }
+            else {
+                r--;
+            }
+        }
+
+        return nums[l];
+    }
+};
