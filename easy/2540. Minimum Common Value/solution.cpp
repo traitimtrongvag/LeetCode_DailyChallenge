45beba3fb5a558a70619a247923d2e38fbfc5a0d
pan2512811 <pan2512811@gmail.com>
2026-05-19 22:15:52 +0700

    feat(leetcode): add C++ solution for 2540. Minimum Common Value

---
+++ easy/2540. Minimum Common Value/solution.cpp
@@ -0,0 +1,28 @@
+/*
+Time: O(n + m)
+Space: O(1)
+
+Idea: two pointers, move smaller value forward
+*/
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return -1;
+    }
+};
