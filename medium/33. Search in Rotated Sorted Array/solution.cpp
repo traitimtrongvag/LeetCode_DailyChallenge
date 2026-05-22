f760b010aef83bf20249bffa4fb4ff51bcb6f9a3
traitimtrongvag <pan2512811@gmail.com>
2026-05-22 22:05:21 +0700

    chore: add comment

--- medium/33. Search in Rotated Sorted Array/solution.cpp
+++ medium/33. Search in Rotated Sorted Array/solution.cpp
@@ -1,25 +1,47 @@
+/*
+Time: O(log n)
+Space: O(1)
+
+Idea: binary search, one half is always sorted
+*/
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
+        int left = 0;
+        int right = nums.size() - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-            
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // left half sorted
             if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
+                // target inside left half
+                if (nums[left] <= target &&
+                    target < nums[mid]) {
+
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[right]) {
+            }
+            // right half sorted
+            else {
+                // target inside right half
+                if (nums[mid] < target &&
+                    target <= nums[right]) {
+
                     left = mid + 1;
                 } else {
                     right = mid - 1;
                 }
             }
         }
+
         return -1;
     }
 };
