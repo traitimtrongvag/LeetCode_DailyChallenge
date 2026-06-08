5444e51e775a457f1390708ceffea81c59e32bd6
pan2512811 <pan2512811@gmail.com>
2026-06-08 22:50:15 +0700

    feat(leetcode): add C++ solution for 2161. Partition Array According to Given Pivot

---
+++ medium/2161. Partition Array According to Given Pivot/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
+        vector<int> ans(n);
+        
+        int left = 0;
+        int right = n - 1;
+        int i = 0, j =n -1;
+        
+        while(i < n){
+            if (nums[i] < pivot) {
+                ans[left++] = nums[i];
+            }
+            
+            if (nums[j] > pivot) {
+                ans[right--] = nums[j];
+            }
+
+            i++;
+            j--;
+        }
+        
+        //remaining indices will be filled with pivot
+        while (left <= right) {
+            ans[left++] = pivot;
+        }
+        
+        return ans;
+    }
+};
